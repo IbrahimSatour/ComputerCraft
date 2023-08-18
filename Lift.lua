ba6aef49493665b1dc9a5c07827f343db0be506f
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2023-08-18 12:40:29 +0300

    Create Lift.lua

---
+++ Lift.lua
@@ -0,0 +1,12 @@
+while true do
+  signal = redstone.getInput("right")
+  if signal == true then
+    sleep(1)
+    level = turtle.detectDown()
+    if level == true then
+      for i=1,3 do turtle.up() end
+    else 
+      for i=1,3 do turtle.down() end
+    end
+  end
+end
