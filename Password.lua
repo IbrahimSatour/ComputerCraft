1e893055955a2474bf963b22b9863bd5bc059ba4
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2023-08-18 12:15:53 +0300

    Update Password.lua

--- Password.lua
+++ Password.lua
@@ -11,17 +11,19 @@ end
 
 -- main command
 
-print("Please enter password")
-input = read("*")
+while true do
+  print("Please enter password")
+  input = read("*")
 
-if input == password then
-  print("Access granted.")
-  redstone.setOutput("back", true)
-  sleep(5)
-  redstone.setOutput("back", false)
-  clear()
-else
-  print("Access denied.")
-  sleep(5)
-  clear()
+  if input == password then
+    print("Access granted.")
+    redstone.setOutput("back", true)
+    sleep(5)
+    redstone.setOutput("back", false)
+    clear()
+  else
+    print("Access denied.")
+    sleep(5)
+    clear()
+  end
 end
