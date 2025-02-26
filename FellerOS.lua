753d179381ba993eaa5e9d20d5837ca80573b0d2
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-26 16:00:42 +0200

    Update FellerOS.lua

--- FellerOS.lua
+++ FellerOS.lua
@@ -1,15 +1,32 @@
+-- variables
+
+local steps = 0
+
 -- functions
 
 function initialPos()
   turtle.dig() turtle.forward()
 end
 
+function stepUp()
+  turtle.dig() turtle.digUp() turtle.up()
+  steps = steps + 1
+end
+
+function stepDown()
+  turtle.digDown() turtle.down() turtle.dig()
+end
+
+function shift()
+  turtle.dig() turtle.turnRight() turtle.dig() turtle.forward() turtle.turnLeft() turtle.dig()
+end
+
+-- main
+
 initialPos()
 
 -- check tree type
-
 local treeType = 1
-
 if turtle.inspect(minecraft:log) then
   treeType = 2
 else
@@ -17,7 +34,6 @@ else
 end
 
 -- treeType 1
-
 if treeType == 1 then
   while turtle.detectUp() do
     turtle.digUp() turtle.up()
@@ -30,4 +46,11 @@ end
 -- treeType 2
 
 if treeType == 2 then
-  
+  while turtle.detectUp() do
+    stepUp()
+  end
+  shift()
+  for i = 1, steps do
+    stepDown()
+  end
+end
