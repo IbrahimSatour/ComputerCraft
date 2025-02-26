fd094dc3c00dc0fcba052b35c355b6e7d12511f9
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-26 18:09:31 +0200

    Update DiggerOS.lua

--- DiggerOS.lua
+++ DiggerOS.lua
@@ -1,4 +1,4 @@
-while not turtle.detectDown("minecraft:bedrock") do
+while not turtle.inspectDown("minecraft:bedrock") do
   for i = 1, 4 do turtle.digDown() turtle.down() end turtle.digDown()
   for i = 1, 2 do turtle.up() end
   turtle.dig() turtle.forward()
