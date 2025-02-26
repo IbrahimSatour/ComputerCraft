f733156304bde687c70a9569e26c8e262a5397ea
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-26 18:20:42 +0200

    Update DiggerOS.lua

--- DiggerOS.lua
+++ DiggerOS.lua
@@ -1,5 +1,11 @@
-while not turtle.inspectDown("minecraft:bedrock") do
+function test()
+  local data = turtle.inspectDown()
+  if data.name == "minecraft:bedrock" then break end
+end
+
+while true do
   for i = 1, 4 do turtle.digDown() turtle.down() end turtle.digDown()
+  test()
   for i = 1, 2 do turtle.up() end
   turtle.dig() turtle.forward()
 end
