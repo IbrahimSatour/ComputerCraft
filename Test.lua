6d3d0d7f2a50cdc621a9d26c7f34cc628678c9bb
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-26 17:31:13 +0200

    Delete Test.lua

--- Test.lua
+++
@@ -1,6 +0,0 @@
-for i=1,16 do
-    turtle.select(i) fill = turtle.getItemCount()
-    if fill ~= 0 then data = turtle.getItemDetail()
-      if data.name == "minecraft:cobblestone" or "minecraft:dirt" or "minecraft:gravel" or "minecraft:sand" then turtle.drop(64) end
-    end
-end
