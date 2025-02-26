07dc7fb1a6bb75ffa3355017c1c8af6a5091a102
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-26 18:25:47 +0200

    Update DiggerOS.lua

--- DiggerOS.lua
+++ DiggerOS.lua
@@ -1,13 +1,12 @@
-function test()
-  local data = turtle.inspectDown()
-  if data.name == "minecraft:bedrock" then
-    os.exit()
-  end
-end
-
 while true do
   for i = 1, 4 do turtle.digDown() turtle.down() end turtle.digDown()
-  test()
+
+  local success, block = turtle.inspectDown()
+        if success and block.name == "minecraft:bedrock" then
+        print("Bedrock detected! Stopping program.")
+        break
+    end
+  
   for i = 1, 2 do turtle.up() end
   turtle.dig() turtle.forward()
 end
