77fa894a9ee2d6cad81718bd116b3f855f62a6e3
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-26 18:03:32 +0200

    Update DiggerOS.lua

--- DiggerOS.lua
+++ DiggerOS.lua
@@ -1,5 +1,7 @@
 while not turtle.detectDown("minecraft:bedrock") do
-  for i = 1, 3 do turtle.digDown() turtle.down() end turtle.digDown()
+  for i = 1, 4 do turtle.digDown() turtle.down() end turtle.digDown()
   for i = 1, 2 do turtle.up() end
   turtle.dig() turtle.forward()
 end
+
+print("Bedrock reached successfully!")
