15cbc51291f3e657d04dc886f7c166d1272088e5
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-27 20:32:19 +0200

    Update TunnelOS2.lua

--- TunnelOS2.lua
+++ TunnelOS2.lua
@@ -10,8 +10,27 @@ function step()
   turtle.digDown()
 end
 
--- user input
+function turnAround()
+  turtle.turnRight()
+  while turtle.detect() do turtle.dig() sleep(0.5) end
+  turtle.forward()
+  while turtle.detectUp() do turtle.digUp() sleep(0.5) end
+  turtle.digDown()
+  turtle.turnRight()
+end
 
+-- user input
 clear() print("Welcome to TunnelOS!") sleep(1) print("Please enter the desired length for your tunnel...")
 local length = read("*")
-clear() print("Tunnel length is set to "..length)
+clear() print("Tunnel length is set to "..length) sleep(1) print("Initiating mining sequence...") sleep(1)
+print("Mining sequence in progress...")
+
+-- main
+initialPos()
+
+for i = 1, length do
+  step()
+end
+
+turnAround()
+
