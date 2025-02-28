ce7340c9dee1525d79e1be3bfc58d80e76e55112
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-28 10:09:02 +0200

    Update PrintOS.lua

--- PrintOS.lua
+++ PrintOS.lua
@@ -9,6 +9,15 @@ if printer.getInkLevel() == 0 then
   error("You're out of ink.")
 end
 
+-- print
+if printer.newPage() then
+  printer.write("a")
+  
+  printer.setCursorPos(1, 3)
+  printer.write("a")
 
-local success = printer.newPage()
-
+  printer.setPageTitle("a")
+  printer.endPage()
+else
+  error("Page could not be created.")
+end
