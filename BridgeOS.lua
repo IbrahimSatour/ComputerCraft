688e53d2bdd28a6dcf21da6df34e4c1a0c23149c
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-02-28 13:36:34 +0200

    Update BridgeOS.lua

--- BridgeOS.lua
+++ BridgeOS.lua
@@ -1,14 +1,16 @@
 -- variables
 local count = turtle.getItemCount(1)
 
--- main
-while true do
-  if count > 0 then
-    turtle.dig()
-    turtle.forward()
-    turtle.select(1)
-    turtle.placeDown()
-  else
-    break
-  end
+-- functions
+function step() turtle.dig() turtle.forward() turtle.select(1) turtle.placeDown() end
+function finish()
+  turtle.turnLeft() step()
+  for i = 1, 3 do turtle.turnRight() step() step() end
+  for i = 1, 2 do turtle.turnRight() step() end
 end
+
+-- main
+while count > 8 do
+  step()
+end
+finish()
