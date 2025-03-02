69aa1d228fe859cae641e5354c2c679c8d0f6849
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-02 19:31:22 +0200

    Update VendingBuyOS.lua

--- VendingBuyOS.lua
+++ VendingBuyOS.lua
@@ -1,3 +1,8 @@
+function clear()
+  term.clear()
+  term.setCursorPos(1, 1)
+end
+
 local type = turtle.getItemDetail(1)
 local count = turtle.getItemCount(1)
 
@@ -10,15 +15,18 @@ if not count == 0 then
       rednet.send(4, count)
       sleep(10)
     else
+      clear()
       print("You can buy a maximum of 16 stacks at a time.")
       sleep(5)
     end
   else
+    clear()
     print("No emeralds found!")
     sleep(5)
   end
 else
+  clear()
   print("No emeralds found!")
-    sleep(5)
+  sleep(5)
 end
 os.reboot()
