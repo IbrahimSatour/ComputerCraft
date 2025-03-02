2e46b4b0ac8bddd04e1452c925257b4b4c6d9f3d
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-02 19:07:17 +0200

    Update VendingGiveOS.lua

--- VendingGiveOS.lua
+++ VendingGiveOS.lua
@@ -1,5 +1,8 @@
 while true do
   rednet.open("right")
-  local order = rednet.receive(3)
-
-  
+  local order = rednet.receive()
+  for i = 1, order do
+    turtle.select(i)
+    turtle.drop(64)
+  end
+end
