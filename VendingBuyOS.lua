4504ec90e9306d9c42c4a9cf0b319775120d999f
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-02 19:07:24 +0200

    Update VendingBuyOS.lua

--- VendingBuyOS.lua
+++ VendingBuyOS.lua
@@ -2,6 +2,7 @@ local type = turtle.getItemDetail(1)
 local count = turtle.getItemCount(1)
 
 if type.name == "minecraft:emerald" then
+  turtle.drop(count)
   rednet.open("right")
   rednet.send(4, count)
 end
