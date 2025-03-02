f729863528ed05fb0f7bff75adffc81e090adbbe
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-02 19:29:54 +0200

    Update VendingBuyOS.lua

--- VendingBuyOS.lua
+++ VendingBuyOS.lua
@@ -2,18 +2,23 @@ local type = turtle.getItemDetail(1)
 local count = turtle.getItemCount(1)
 
 term.clear()
-if type.name == "minecraft:emerald" then
-  if count < 17 then
-    turtle.drop(count)
-    rednet.open("right")
-    rednet.send(4, count)
-    sleep(10)
+if not count = 0 then
+  if type.name == "minecraft:emerald" then
+    if count < 17 then
+      turtle.drop(count)
+      rednet.open("right")
+      rednet.send(4, count)
+      sleep(10)
+    else
+      print("You can buy a maximum of 16 stacks at a time.")
+      sleep(5)
+    end
   else
-    print("You can buy a maximum of 16 stacks at a time.")
+    print("No emeralds found!")
     sleep(5)
   end
 else
   print("No emeralds found!")
-  sleep(5)
+    sleep(5)
 end
 os.reboot()
