9635e0f0d81d0f4d1733ee16ba17373ef3426b63
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-03 17:48:18 +0200

    Update VendingBuyOS.lua

--- VendingBuyOS.lua
+++ VendingBuyOS.lua
@@ -1,4 +1,23 @@
-function clear() term.clear() term.setCursorPos(1, 1) end
+-- variables
 
 local count = turtle.getItemCount(1)
 local type = turtle.getItemDetail(1)
+local stock = false
+
+-- functions
+
+function clear() term.clear() term.setCursorPos(1, 1) end
+
+function checkStock()
+  rednet.open("left")
+  rednet.send("check")
+  if rednet.receive() == yes then
+    stock = true
+  end
+end
+
+function makeTrade()
+  turtle.drop(1)
+  rednet.open("right")
+  rednet.send(1, count)
+end
