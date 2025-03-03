8a5c77c617e622e69972943474ee5478bc10168a
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-03 18:41:21 +0200

    Update VendingGiveOS.lua

--- VendingGiveOS.lua
+++ VendingGiveOS.lua
@@ -5,7 +5,12 @@ function clear() term.clear() term.setCursorPos(1, 1) end
 function checkStock()
   local senderId, message = rednet.receive()
   if message == "check" then
-    print("received")
+    for 1, 16 do
+      if turtle.getItemCount(i) < 64 then
+        rednet.open("right")
+        rednet.send("no")
+      end
+    end
   end
 end
 
