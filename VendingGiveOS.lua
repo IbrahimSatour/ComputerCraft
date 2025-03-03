e781267b453d21737779d27ca0de4721e46ebc20
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-03 18:24:33 +0200

    Update VendingGiveOS.lua

--- VendingGiveOS.lua
+++ VendingGiveOS.lua
@@ -1,12 +1,17 @@
+-- functions
+
+function clear() term.clear() term.setCursorPos(1, 1) end
+
+-- main
+
+clear()
+
 rednet.open("right")
 if rednet.receive() == "check" then
   for i = 1, 16 do
-    if not turtle.getItemDetail(i) == 64 then
+    if turtle.getItemDetail(i) < 64 then
       rednet.open("right")
       rednet.send(0, "no")
-    else
-      rednet.open("right")
-      rednet.send(0, "yes")
     end
   end
 end
