1fec44ef23cef66b56f125e185e38202fc9af432
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-03 18:11:11 +0200

    Update VendingGiveOS.lua

--- VendingGiveOS.lua
+++ VendingGiveOS.lua
@@ -1,11 +1,11 @@
-rednet.open("left")
+rednet.open("right")
 if rednet.receive() == "check" then
   for i = 1, 16 do
     if not turtle.getItemDetail(i) == 64 then
-      rednet.open("left")
+      rednet.open("right")
       rednet.send(0, "no")
     else
-      rednet.open("left")
+      rednet.open("right")
       rednet.send(0, "yes")
     end
   end
