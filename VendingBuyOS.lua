9994c9310c8ec843a0c9666c98528b1662c3c446
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-04 23:38:41 +0200

    Update VendingBuyOS.lua

--- VendingBuyOS.lua
+++ VendingBuyOS.lua
@@ -8,47 +8,44 @@ local stock = true
 
 function clear() term.clear() term.setCursorPos(1, 1) end
 
-function checkStock()
-  rednet.open("right")
-  rednet.send(1, "check")
-  local senderId, message = rednet.receive(nil, 1)
-  if message == "no" then
-    stock = false
-  end
-end
-
-function makeTrade()
-  turtle.drop()
-  rednet.open("right")
-  rednet.send(1, count)
-end
-
 -- main
 
-checkStock()
+rednet.open("right")
 
-if stock == true then
-  if count > 0 then
-    if type.name == "minecraft:emerald" then
-      if count < 17 then
-        rednet.send(1, "noError")
-        makeTrade()
-        local name1 = "Purchasing"
-        local name2 = "stacks..."
-        clear() print(name1, count, name2) sleep(5) clear()
-      else
-        clear() print("You can only buy a maximum of 16 stacks at a time!")
-        sleep(5) os.reboot()
-      end
-    else
-      clear() print("You can only buy using emeralds!")
-      sleep(5) os.reboot()
-    end
-  else
-    clear() print("No emeralds were found in the first slot!")
-    sleep(5) os.reboot()
-  end
-else
+rednet.send(1, "check")
+local senderId, message = rednet.receive(nil, 1)
+if message == "no" then
+  stock = false
+end
+
+if stock == false then
+  rednet.send(1, count)
+  rednet.send(1, "noTrade")
   clear() print("The shop is out of stock! Sorry for the inconvenience.")
   sleep(5) os.reboot()
 end
+
+if count == 0 then
+  rednet.send(1, count)
+  rednet.send(1, "noTrade")
+  clear() print("No emeralds were found in the first slot!")
+  sleep(5) os.reboot()
+end
+
+if not (type.name == "minecraft:emerald") then
+  rednet.send(1, count)
+  rednet.send(1, "noTrade")
+  clear() print("You can only buy using emeralds!")
+  sleep(5) os.reboot()
+end
+
+if count > 16 then
+  rednet.send(1, count)
+  rednet.send(1, "noTrade")
+  clear() print("You can only buy a maximum of 16 stacks at a time!")
+  sleep(5) os.reboot()
+end
+
+turtle.drop()
+count = tonumber(count)
+rednet.send(1, count)
