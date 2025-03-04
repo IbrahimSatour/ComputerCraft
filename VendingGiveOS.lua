f7d5b6976780d5420677af4ace88da099a869f3a
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-04 23:38:45 +0200

    Update VendingGiveOS.lua

--- VendingGiveOS.lua
+++ VendingGiveOS.lua
@@ -1,46 +1,40 @@
 -- variables
 
 local stock = true
+local trade = true
 
 -- functions
 
 function clear() term.clear() term.setCursorPos(1, 1) end
 
-function checkStock()
-  local senderId, message = rednet.receive()
-  if message == "check" then
-    for i = 1, 16 do
-      if turtle.getItemCount(i) < 64 then
-        rednet.open("right")
-        rednet.send(0, "no")
-        stock = false
-      end
+-- main
+
+rednet.open("right")
+
+local senderId, message = rednet.receive()
+if message == "check" then
+  for i = 1, 16 do
+    if turtle.getItemCount(i) < 64 then
+      rednet.send(0, "no")
+      stock = false
     end
   end
 end
 
-function makeTrade()
-  local senderID, message = rednet.receive()
-  local count = tonumber(message)
-  for i = 1, count do
-    turtle.select(i)
-    turtle.dropDown()
-  end
-  local name1 = "Sold"
-  local name2 = "stacks."
-  print(name1, num, name2)
+if stock == false then
+  os.reboot()
 end
 
--- main
-
-while true do
-
-  rednet.open("right")
-
-  checkStock()
-
-  if stock == true then
-    makeTrade()
-  end
-
+local senderId, message = rednet.receive(nil, 1)
+if message == "noTrade" then
+  trade = false
 end
+
+local senderID, message = rednet.receive()
+local count = message
+for i = 1, count do
+  turtle.select(i)
+  turtle.dropDown()
+end
+
+os.reboot()
