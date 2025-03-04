e662d45514f7b4ffd7062ac28650ecf55a1225a0
IbrahimSatour <142263616+IbrahimSatour@users.noreply.github.com>
2025-03-05 01:33:19 +0200

    Create VendingGiveOS2.lua

---
+++ VendingGiveOS2.lua
@@ -0,0 +1,36 @@
+local trade = true
+
+rednet.open("right")
+
+-- receiving count
+local senderId, message = rednet.receive()
+local count = message
+if count > 16 then
+  count = 16
+end
+
+-- checking stock
+local senderId, message = rednet.receive()
+if message == "checkStock" then
+  for i = 1, 16 do
+    if turtle.getItemCount(i) < 64 then
+      rednet.send(0, "noStock")
+    end
+  end
+end
+
+-- filtering
+local senderId, message = rednet.receive(nil, 2)
+if message == "noTrade" then
+  trade = false
+end
+
+-- trade
+if trade == true then
+  for i = 1, count do
+    turtle.select(i)
+    turtle.dropDown()
+  end
+end
+
+os.reboot()
